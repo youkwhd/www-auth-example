db16234b2f15f2704a5847a98cee9e36ebdd0115
youkwhd <lolywk@tutanota.com>
2023-08-24 01:02:41 +0700

    feat(sessions): keep track of expiry

--- db/session/session.go
+++ db/session/session.go
@@ -1,20 +1,38 @@
 package session
 
 import (
-    "www-auth-example/db/user"
+	"errors"
+	"time"
+	"www-auth-example/db/user"
 )
 
 type Session struct {
     Id string
     User *user.User
+    ExpiresIn time.Time
 }
 
 type Sessions map[string]Session
 
-func (u *Sessions) Add(id string, user *user.User) {
-    (*u)[id] = Session {
+func (s *Sessions) Get(id string) (*Session, error) {
+    session, found := (*s)[id]
+
+    if !found {
+        return nil, errors.New("Session not found")
+    }
+
+    if time.Now().Sub(session.ExpiresIn) <= 0 {
+        return nil, errors.New("Session is expired")
+    }
+
+    return &session, nil
+}
+
+func (s *Sessions) Add(id string, user *user.User, expiresIn time.Time) {
+    (*s)[id] = Session {
         Id: id,
         User: user,
+        ExpiresIn: expiresIn,
     }
 }
 
